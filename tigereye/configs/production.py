a5f8348f7807eb1e2761ada89f53f923afbabc79
guye <guye999@gmail.com>
2018-01-20 17:52:23 +0800

    add email config

--- tigereye/configs/production.py
+++ tigereye/configs/production.py
@@ -10,3 +10,10 @@ class ProductionConfig(DefaultConfig):
     JSONIFY_PRETTYPRINT_REGULAR = False
     SQLALCHEMY_ECHO = False
     SEND_LOGS = True
+
+    EMAIL_HOST = 'smtp.exmail.qq.com'
+    EMAIL_PORT = 465
+    EMAIL_HOST_USER = SERVER_EMAIL = DEFAULT_FROM_EMAIL = 'test1@iguye.com'
+    EMAIL_HOST_PASSWORD = 'P67844QUssW3'
+    EMAIL_USE_SSL = True
+    ADMINS = ["guye@iguye.com"]
